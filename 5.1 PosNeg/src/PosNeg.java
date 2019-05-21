1adf9b9bbcc15ffe276c235fd249b267619b1400
Suraj Upadhyay <37338518+upadhyaysuraj@users.noreply.github.com>
2019-05-21 13:01:05 -0400

    Add files via upload

---
+++ 5.1 PosNeg/src/PosNeg.java
@@ -0,0 +1,88 @@
+//SURAJ UPADHYAY
+//CSC 161-01
+//LAB 5 #1
+
+import java.util.Scanner;
+
+public class PosNeg 
+{
+    public static void main(String[] args) 
+    {
+        //Initialising Scanner.
+        Scanner key = new Scanner(System.in);
+        
+        //Asking the user to enter a double value.
+        System.out.print("Please enter a double type number: ");
+        
+        //Storing the value.
+        double num = key.nextDouble();
+        
+        //Comparing the user entered value with 0.
+        if(num == 0)
+        {
+            System.out.println("The number you entered is zero.");
+        }
+        else if (num < 0)
+        {
+            System.out.println("The number you entered is negative.");
+        }
+        else
+        {
+            System.out.println("The number you entered is positive.");
+        }
+    }
+    
+    public static void main1(String[] args) 
+    {
+        //Initialising Scanner.
+        Scanner key = new Scanner(System.in);
+        
+        //Asking the user to enter a double value.
+        System.out.print("Please enter a double type number: ");
+        
+        //Storing the value.
+        double num = key.nextDouble();
+        
+        //Comparing the user entered value with 0.
+        if(num == 0)
+        {
+            System.out.println("The number you entered is zero.");
+        }
+        else if (num < 0)
+        {
+            System.out.println("The number you entered is negative.");
+        }
+        else
+        {
+            System.out.println("The number you entered is positive.");
+        }
+    }
+    
+    public static void main3(String[] args) 
+    {
+        //Initialising Scanner.
+        Scanner key = new Scanner(System.in);
+        
+        //Asking the user to enter a double value.
+        System.out.print("Please enter a double type number: ");
+        
+        //Storing the value.
+        double num = key.nextDouble();
+        
+        //Comparing the user entered value with 0.
+        if(num == 0)
+        {
+            System.out.println("The number you entered is zero.");
+        }
+        else if (num < 0)
+        {
+            System.out.println("The number you entered is negative.");
+        }
+        else
+        {
+            System.out.println("The number you entered is positive.");
+        }
+    }
+    
+    
+}
